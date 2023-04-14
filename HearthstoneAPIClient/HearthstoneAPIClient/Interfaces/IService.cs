7894c35a5c9d6f12fa567347d2d0fc6a983cc6b6
Lucas Brennan <lucasjacbrennan@gmail.com>
2023-04-14 12:20:08 +0100

    Framework

---
+++ HearthstoneAPIClient/HearthstoneAPIClient/Interfaces/IService.cs
@@ -0,0 +1,18 @@
+﻿using HearthstoneAPIClient.Services.DataHandling;
+using Newtonsoft.Json.Linq;
+
+namespace HearthstoneAPIClient.Interfaces
+{
+    public interface IService
+    {
+        #region Properties
+        public CallManager Manager { get; set; }
+
+        public JObject ResponseContent { get; set; }
+
+        public IResponse ResponseObject { get; set; }
+        #endregion
+
+        public Task MakeRequestAsync(string requestString);
+    }
+}
---
+++ HearthstoneAPIClient/HearthstoneAPITests/AppConfigReader.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace HearthstoneAPITests
+{
+    public static class AppConfigReader
+    {
+        public static readonly string BaseUrl = ConfigurationManager.AppSettings["base_url"];
+        public static readonly string ApiKey = ConfigurationManager.AppSettings["api_key"];
+        public static readonly string ApiHost = ConfigurationManager.AppSettings["api_host"];
+        public static readonly string ApiCookie = ConfigurationManager.AppSettings["api_cookie"];
+    }
+}
